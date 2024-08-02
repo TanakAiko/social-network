755a325bd3da4954600761ac9e835ffbf55a7c60
TanakAiko <cherifmbaye02@gmail.com>
2024-08-02 19:25:17 +0000

    first commit

--- script/push.sh
+++ script/push.sh
@@ -4,6 +4,6 @@ echo "Enter the message for the commit"
 read message
 git add $files
 git config --global user.email cherifmbaye02@gmail.com
-git config --global user.name cheimbaye
+git config --global user.name TanakAiko
 git commit -m "$message"
 git push
